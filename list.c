27af4c21eab5067461950f721d28dc9d0a0b118f
Soldour <56756863+Soldour@users.noreply.github.com>
2020-03-15 00:27:18 +0200

    Update list.c

--- list.c
+++ list.c
@@ -13,51 +13,26 @@ void creatLinkList(int n);
 void deletAllElement(int  key);
 void deletElemntByIndex(int index);
 _Bool get_Element_ByValue(int elemnt);
-
-
-
-_Bool deletElemntByKey( int key)
-{
-	struct node* indexing_Node = head;
-
-	int count = 0;
-	if (indexing_Node->data == key) {
-		printf("\nthe element is at the head so no can do :\n");
-		return false;
-
-	}
-
-	else {
-		while (indexing_Node->next->data != key)
-		{
-			indexing_Node = indexing_Node->next;
-			count++;
-
-		}
-		indexing_Node->next = indexing_Node->next->next;
-		printf("the value %d is deleted form the list\n", key);
-
-	}
-
-}
-
-
-
-
+void printList();
+int get_Element(int index_of_element);
+void insertElement(int value, int bound);
+struct node* begning(int value);
 
 
 int main()
 {
 
 	
-
-	int n = 4;
+	int n = 5;
 
 	creatLinkList(n);
-
-	//get_Element_ByValue(22) ? printf("\nYes\n") : printf("the element is not a memeber of this list ;-( \n");
-
-	//deletElemntByKey(head, 22);
-	deletAllElement( 33);
+	printList(); 
+	head=begning( 95858);
+	printList();
+	insertElement( 900,n);	
+	deletElemntByIndex(2);
+	printList();
+	deletAllElement(33);     //calling function that will delete all node with the matching data field as inputt
+	printList();
 
 }
